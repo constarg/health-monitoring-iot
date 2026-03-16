3aa199994e5d95f3204fb14c0ab79912116d8dda
Constantinos Argyriou <argyrioukost@gmail.com>
2026-03-17 00:43:02 +0200

    Add the LICENCE header in source files.

--- include/health_monitor.h
+++ include/health_monitor.h
@@ -1,3 +1,27 @@
+/**
+ * File: blakes2.h
+ *
+ ***********************************************************************
+ * Contains MACROS and classes definitions used for the monitoring system.
+ *
+ * Copyright (C) 2026  Constantinos Argyriou
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Email: constarg@pm.me
+ ***********************************************************************
+ */
 #ifndef IOT_MSC_APPLICATION_H
 #define IOT_MSC_APPLICATION_H
 
--- include/moving_common.h
+++ include/moving_common.h
@@ -1,3 +1,29 @@
+/**
+ * File: moving_common.h
+ *
+ ***********************************************************************
+ * Contains MACROS and structure definitions used across the proeject.
+ * Most of the defined MACROS are used to determine the movement thresholds,
+ * such as when the user walks, runs, jumps, falls and stays.
+ *
+ * Copyright (C) 2026  Constantinos Argyriou
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Email: constarg@pm.me
+ ***********************************************************************
+ */
 #ifndef MOVING_COMMON_H
 #define MOVING_COMMON_H
 
